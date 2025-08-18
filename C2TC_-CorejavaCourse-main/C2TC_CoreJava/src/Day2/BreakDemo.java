876266bbc9faef383e101f34e5493ed85d725799
Lenovo <Lenovo@LAPTOP-DC4N3PUC>
2025-08-18 15:06:43 +0530

    commit

---
+++ C2TC_-CorejavaCourse-main/C2TC_CoreJava/src/Day2/BreakDemo.java
@@ -0,0 +1,12 @@
+package Day2;
+
+public class BreakDemo {
+	public static void main(String[] args) {
+
+		for (int i = 1; i < 10; i++) {
+			if (i == 5)
+				break;
+			System.out.print(i+" ");
+		}
+	}
+}
---
+++ C2TC_-CorejavaCourse-main/C2TC_CoreJava/src/Day2/IfElseStatements.java
@@ -0,0 +1,13 @@
+package Day_2;
+
+public class IfElseStatements {
+    	public static void main(String[] args) {
+		int mark=35;
+		
+		if(mark>=35)
+			System.out.println("your pass");	
+		else
+			System.out.println("your fail");
+
+}
+}
