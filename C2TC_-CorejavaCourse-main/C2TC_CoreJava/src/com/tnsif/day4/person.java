4cc09488bead70da91ea8404e6262d5deb0fc2cf
Lenovo <Lenovo@LAPTOP-DC4N3PUC>
2025-08-18 18:56:19 +0530

    Commit

---
+++ C2TC_-CorejavaCourse-main/C2TC_CoreJava/src/com/tnsif/day4/person.java
@@ -0,0 +1,45 @@
+package com.tnsif.day4;
+
+public class person {
+	private String personName;
+	private int personAge;
+	private String personCity;
+	
+	public String getPersonName() {
+		return personName;
+	}
+
+	public void setPersonName(String personName) {
+		this.personName = personName;
+	}
+
+	public int getPersonAge() {
+		return personAge;
+	}
+
+	public void setPersonAge(int personAge) {
+		this.personAge = personAge;
+	}
+
+	public String getPersonCity() {
+		return personCity;
+	}
+
+	public void setPersonCity(String personCity) {
+		this.personCity = personCity;
+	}
+
+	public person() {
+		System.out.println("Default Constructor");
+	}
+	
+	public person(String personName, int personAge, String personCity){
+		System.out.println("Parameterized Constructor");
+		this.personName = personName;
+		this.personAge = personAge;
+		this.personCity = personCity;
+	}
+	
+	
+}
+
